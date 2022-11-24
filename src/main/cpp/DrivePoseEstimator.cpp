204d60419385e4d8da55ea38684ef2879c182b31
Gabor Szita <gaborszita88@gmail.com>
2022-11-23 18:28:43 -0800

    Plan for PhotonVision pose estimation

--- src/main/cpp/DrivePoseEstimator.cpp
+++ src/main/cpp/DrivePoseEstimator.cpp
@@ -1,4 +1,5 @@
 #include "DrivePoseEstimator.h"
+#include "Constants.h"
 
 DrivePoseEstimator::DrivePoseEstimator(photonlib::PhotonCamera& photonCamera, SwerveDrive& swerveDrive, AHRS& navx) {
     this->swerveDrive = &swerveDrive;
@@ -12,13 +13,34 @@ DrivePoseEstimator::DrivePoseEstimator(photonlib::PhotonCamera& photonCamera, Sw
 */
 void DrivePoseEstimator::update() {
     estimator.Update(navx->GetRotation2d(), swerveDrive->getRealModuleStates());
-    // TODO: figure out how to add camera measurement
     //estimator.AddVisionMeasurement(limelight->getPose(0.0, 0.0), frc::Timer::GetFPGATimestamp() - 0.3_s);
-    //auto res = photonCamera->GetLatestResult();
-    //if (res.HasTargets()) {
-    //    auto imageCaptureTime = res.GetTimestamp();
-    //    auto camTargetTrans = res.GetBestTarget().GetBestCameraToTarget();
-    //    auto camPose = 
-    //}
 
+    /*
+    TODO: figure out how to add PhotonVision camera measurement
+    This is not yet incorporated in PhotonVision C++, we have to wait until they get it out.
+    You can see on [this](https://docs.photonvision.org/en/latest/docs/examples/simposeest.html) page that
+    the C++ code sections have "Coming soon!" for the time being.
+    I created a draft for how it will get done using the Java code samples - however, this may
+    require some changes as the C++ method names and signatures may be a little different.
+    It's commented it out becaues it won't compile.
+    */
+
+    /*auto res = photonCamera->GetLatestResult();
+    if (res.HasTargets()) {
+        auto imageCaptureTime = res.GetTimestamp();
+        frc::Transform2d camToTargetTrans = res.GetBestTarget().GetCameraToTarget();
+        frc::Pose2d camPose = GeneralConstants::FAR_TARGET_POSE.TransformBy(camToTargetTrans.Inverse());
+        estimator.AddVisionMeasurement(camPose.TransformBy(LimelightConstants::CAMERA_TO_ROBOT), imageCaptureTime);
+    }*/
+
+    /*
+    Plan for constants:
+    in namespace GeneralConstants:
+    const frc::Pose2d FAR_TARGET_POSE;
+
+    in namespace LimelightConstants:
+    const frc::Transform3d CAMERA_TO_ROBOT(
+        frc::Translation3d(units::meter_t(ROBOT_TURRET_CENTER_DISTANCE), units::meter_t(0.0), 
+        units::meter_t(ROBOT_TURRET_CENTER_DISTANCE)), frc::Rotation3d());
+    */
 }
